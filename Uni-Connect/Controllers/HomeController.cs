790bc09b3abcccbe7d491ada5713339167bdfe0f
Ahmad-Allahawani <ahmad.allahawani04@gmail.com>
2026-03-19 21:51:38 +0300

    Applying the front-end design for (Home,Login,Register,forgetpass)

--- Uni-Connect/Controllers/HomeController.cs
+++ Uni-Connect/Controllers/HomeController.cs
@@ -17,6 +17,9 @@ namespace Uni_Connect.Controllers
         {
             return View();
         }
+        
+
+
 
         public IActionResult Privacy()
         {
---
+++ Uni-Connect/Controllers/LoginController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Uni_Connect.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login_Page()
+        {
+            return View();
+        }
+        public IActionResult Register_Page()
+        {
+            return View();
+        }
+        public IActionResult ForgotPass_Page()
+        {
+            return View();
+        }
+    }
+}
