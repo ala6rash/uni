0408f18713b5e3e0c6289f0c5cddf93c5dbe34e7
UniConnect Developer <developer@uniconnect.edu>
2026-04-04 20:18:24 +0300

    feat: Add full project structure and remaining files to repository

---
+++ Downloads/Uni-Connect-master/Uni-Connect/Controllers/DashboardController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Uni_Connect.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+        public IActionResult Profile()
+        {
+            return View();
+        }public IActionResult Notifications()
+        {
+            return View();
+        } 
+        public IActionResult Leaderboard()
+        {
+            return View();
+        }
+        
+    }
+}
